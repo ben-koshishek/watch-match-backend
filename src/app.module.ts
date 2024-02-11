cbe15944aa18fc95589ac81a4f6a140479271ce0
Mykyta Yunoshev <mykyta.yunoshev@lipsia.digital>
2024-02-11 04:11:31 +0100

    Update database configuration in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,11 +20,13 @@ import { User } from './user/user.entity';
       useFactory: (configService: ConfigService) => {
         return {
           type: 'postgres',
-          host: configService.get('DB_HOST'),
-          port: +configService.get<number>('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
+          host:
+            configService.get('DB_HOST') ||
+            'database-1.cluster-ro-cxc8606wu601.eu-central-1.rds.amazonaws.com',
+          port: +configService.get<number>('DB_PORT') || 5432,
+          username: configService.get('DB_USERNAME') || 'postgres',
+          password: configService.get('DB_PASSWORD') || 'L8cfddjecB7D^HFY',
+          database: configService.get('DB_DATABASE') || 'postgres',
           entities: [User],
           synchronize: process.env.ENV === 'dev' ? true : false,
           autoLoadEntities: true,
