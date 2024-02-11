55fd3ba2de0f2dd9a91c6448b91d4f2dab17b994
Mykyta Yunoshev <mykyta.yunoshev@lipsia.digital>
2024-02-11 04:32:43 +0100

    Add console log for database environment variables

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env.DB_HOST, process.env.DB_NAME)
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
