3fd16f4b1cb632eed2f44e58cf3930ef576741c6
Mykyta Yunoshev <mykyta.yunoshev@lipsia.digital>
2024-02-11 04:32:48 +0100

    Update console.log statements to console.error

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.DB_HOST, process.env.DB_NAME)
-  
+  console.error(process.env.DB_HOST, process.env.DB_NAME)
+  console.error('qweqwqwe');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
